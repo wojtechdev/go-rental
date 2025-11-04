cb106de4d1338de427d438b349ecb897378c0ec6
theWebCookie <jabwojtek@wp.pl>
2025-11-04 17:01:53 +0100

    feat(server): create car model

---
+++ server/models/car.model.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const carSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: [true, 'Please enter car name'] },
+    description: { type: String, required: [true, 'Please enter car description'] },
+    status: { type: String, default: 'Draft' },
+    rentPerDay: { type: Number, required: [true, 'Please enter rent per day'] },
+    address: { type: String, required: [true, 'Please enter address'] },
+    images: [{ url: String, public_id: String }],
+    brand: { type: String, required: [true, 'Please enter car brand'] },
+    year: { type: Number, required: [true, 'Please enter car year'] },
+    transmission: { type: String, required: [true, 'Please enter transmission type'] },
+    milleage: { type: Number, required: [true, 'Please enter car mileage'] },
+    power: { type: Number, required: [true, 'Please enter car power'] },
+    seats: { type: Number, required: [true, 'Please enter number of seats'] },
+    doors: { type: Number, required: [true, 'Please enter number of doors'] },
+    fuelType: { type: String, required: [true, 'Please enter fuel type'] },
+    category: { type: String, required: [true, 'Please enter car category'] },
+    reviews: [String],
+  },
+  { timestamps: true }
+);
+
+const Car = mongoose.model('Car', carSchema);
+
+export default Car;
