f2c9ca726cb341878bfec5c4e854efdf99eee4f1
theWebCookie <jabwojtek@wp.pl>
2025-11-05 19:37:25 +0100

    feat(client): add heade and footer

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,3 @@
-import { Button } from '@/components/ui/button';
-import { ArrowUpIcon } from 'lucide-react';
-
 export default function Home() {
-  return (
-    <div className='flex flex-wrap items-center gap-2 md:flex-row'>
-      <Button variant='outline'>Button</Button>
-      <Button variant='outline' size='icon' aria-label='Submit'>
-        <ArrowUpIcon />
-      </Button>
-    </div>
-  );
+  return <div></div>;
 }
